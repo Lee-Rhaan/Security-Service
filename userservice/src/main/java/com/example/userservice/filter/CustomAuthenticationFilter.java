4d7fad307c41d6ff4a09765488bb853e75b78097
Lee-Rhaan <rhaanking@gmail.com>
2021-10-25 10:30:23 +0200

    Configured BCrypt and UserDetails

---
+++ userservice/src/main/java/com/example/userservice/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,6 @@
+package com.example.userservice.filter;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+}
